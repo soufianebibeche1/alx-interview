7514de3a4569299ff604a4f8d39a63f410bb6c0c
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-06-19 01:31:59 +0000

    add 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-Implementing making changes challenge
+0-making_change.py
 """
 
 
 def makeChange(coins, total):
     """
-    implement making changes
+    fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
 
-    # Initialize a list to store the minimum
-    # 3number of coins needed for each coin denomination.
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # It takes 0 coins to make a total of 0.
+    dp[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            # Update dp[i] with the minimum of dp[i] and dp[i - coin] + 1
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] is still infinity, it means the total cannot be met.
-    # Otherwise, return the minimum number of coins needed.
     return dp[total] if dp[total] != float('inf') else -1
